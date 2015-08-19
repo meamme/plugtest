42f9ae556555daad44e8b73453b2c1dc27017763
meamme <meamme@users.noreply.github.com>
2015-08-19 17:59:18 -0500

    Update mmm_script.js

--- mmm_script.js
+++ mmm_script.js
@@ -2,6 +2,7 @@
 
 var isstopped = 0;
 var usertoggle = 0;
+var waittime = 1000;
 
 document.getElementById("facebook-menu").innerHTML="<a style='color:red' onclick='clickbutton()'  href='#'>togg is off</a>"
 
@@ -96,7 +97,27 @@ if (value == "/stop") {
   isstopped = 1;
   API.sendChat("/me stopped script :(");
 }
+
+if (value == "/katy") {
+	
+	API.sendChat("hi every1 im new!!!!!!! holds up spork my name is katy but u can call me t3h PeNgU1N oF d00m!!!!!!!!"); 
+	setTimeout(waittime);
+	API.sendChat("lol…as u can see im very random!!!! thats why i came here, 2 meet random ppl like me _… im 13 years old (im mature 4 my age tho!!)");
+	setTimeout(waittime);
+	API.sendChat("i like 2 watch invader zim w/ my girlfreind (im bi if u dont like it deal w/it) its our favorite tv show!!! bcuz its SOOOO random!!!!");
+	setTimeout(waittime);
+	API.sendChat("shes random 2 of course but i want 2 meet more random ppl =) like they say the more the merrier!!!!");
+	setTimeout(waittime);
+	API.sendChat("lol…anyways i hope 2 make alot of freinds here so give me lots of commentses!!!!");
+	setTimeout(waittime);
+	API.sendChat("DOOOOOMMMM!!!!!!!!!!!!!!!! <--- me bein random again _^ hehe…toodles!!!!!");
+
+
 }
+
+}
+//end ifstopped
+
 }
 
 function clickbutton() {
